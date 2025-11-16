ee8c161a25ecc7a7363ad8a8aa8bc0cf89222972
ntzsbiel <gabrielsilva.ilha186@gmail.com>
2025-11-16 15:04:31 -0300

    refactor (entities): reorganiza estrutura do pacote

---
+++ projeto-spring-boot/projeto-spring-boot/src/main/java/com/produtoapi/projeto_spring_boot/entities/Categoria.java
@@ -0,0 +1,28 @@
+package com.produtoapi.projeto_spring_boot.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data                       // Gera getters, setters, toString, equals e hashCode
+@NoArgsConstructor          // Construtor vazio
+@AllArgsConstructor         // Construtor com todos os atributos
+
+public class Categoria {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	
+	@NotBlank(message = "O nome da categoria é obrigatório")
+    @Size(min = 3, max = 50, message = "O nome deve ter entre 3 e 50 caracteres")
+	private String nome;
+	
+}
