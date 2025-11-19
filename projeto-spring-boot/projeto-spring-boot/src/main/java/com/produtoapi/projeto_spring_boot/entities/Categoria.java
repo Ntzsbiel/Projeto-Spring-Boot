7169f5656e61c133efa503f324b17d0ea55644c9
joseborges2025’ <jneto2901@gmail.com>
2025-11-19 10:26:23 -0300

    feat service: Criação service categoria

--- projeto-spring-boot/projeto-spring-boot/src/main/java/com/produtoapi/projeto_spring_boot/entities/Categoria.java
+++ projeto-spring-boot/projeto-spring-boot/src/main/java/com/produtoapi/projeto_spring_boot/entities/Categoria.java
@@ -24,6 +24,23 @@ public class Categoria {
 	@NotBlank(message = "O nome da categoria é obrigatório")
     @Size(min = 3, max = 50, message = "O nome deve ter entre 3 e 50 caracteres")
 	private String nome;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
 	
 	
 }
--- projeto-spring-boot/projeto-spring-boot/src/main/java/com/produtoapi/projeto_spring_boot/services/CategoriaService.java
+++ projeto-spring-boot/projeto-spring-boot/src/main/java/com/produtoapi/projeto_spring_boot/services/CategoriaService.java
@@ -1,15 +1,53 @@
 package com.produtoapi.projeto_spring_boot.services;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.produtoapi.projeto_spring_boot.entities.Categoria;
 import com.produtoapi.projeto_spring_boot.repositories.CategoriaRepository;
+
 
 @Service
 
 public class CategoriaService {
 	
 	@Autowired
-	private CategoriaRepository repository;
+	private CategoriaRepository categoriarepo;
+	
+	public List<Categoria> procuraTodos()
+	{
+		return categoriarepo.findAll(); 
+	}
+	
+	public Categoria procurarPorId(Integer id)
+	{
+		return categoriarepo.findById(id).get();
+	}
 
-}
+	public String adicionarCategoria(Categoria categoria)
+	{
+		categoriarepo.save(categoria);
+		return " Categoria adicionada com sucesso !";
+	}
+	
+	public String editarCategoria(Integer id, Categoria categoria)
+	{
+		Categoria response = categoriarepo.findById(id).get();
+		
+		response.setId(categoria.getId());
+		response.setNome(categoria.getNome());
+		
+		categoriarepo.save(response);
+		return " Categoria editada com sucesso! ";
+	}	
+	public void excluirCategoria(Integer id)
+	{
+		Categoria response = categoriarepo.findById(id).get();
+		categoriarepo.delete(response);
+	}
+		
+		
+	}
+
